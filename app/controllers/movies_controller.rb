394171dcb102bf1eb7403c899c1245920ff3bf88
okok111 <1for.rails@gmail.com>
2023-02-19 22:35:51 +0900

    kaminariにてページネーション実装＆取得件数を100

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,11 +3,28 @@ class MoviesController < ApplicationController
   def index
     if params[:looking_for]
       movie_title = params[:looking_for]
-      url = "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API']}&language=ja&query=" + URI.encode_www_form_component(movie_title)
+      movies = []
+      (1..5).each do |page|
+        url = "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API']}&language=ja&query=" + URI.encode_www_form_component(movie_title) + "&page=#{page}"
+        response = Net::HTTP.get_response(URI.parse(url))
+        if response.code == "200"
+          result = JSON.parse(response.body)
+          movies.concat(result["results"])
+        end
+      end
+      @movies = Kaminari.paginate_array(movies).page(params[:page]).per(10)
     else
-      url = "https://api.themoviedb.org/3/movie/popular?api_key=#{ENV['TMDB_API']}&language=ja"
+      movies = []
+      (1..5).each do |page|
+        url = "https://api.themoviedb.org/3/movie/popular?api_key=#{ENV['TMDB_API']}&language=ja&page=#{page}"
+        response = Net::HTTP.get_response(URI.parse(url))
+        if response.code == "200"
+          result = JSON.parse(response.body)
+          movies.concat(result["results"])
+        end
+      end
+      @movies = Kaminari.paginate_array(movies).page(params[:page]).per(10)
     end
-    @movies = JSON.parse(Net::HTTP.get(URI.parse(url)))
   end
 
   def show
